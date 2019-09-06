9e650018a4f88329b8f259b7f709bdf6feae8654
Adi Bhaktaram <adithyabhaktaram@casady.org>
2019-09-06 09:19:32 -0500

    more notes

--- stamp.py
+++ stamp.py
@@ -9,6 +9,15 @@ def stamper(t, size):
         t.fd(size)
         t.rt(24)
 
+def drawColors(t):
+
+    for _ in range(30):
+        myColor = random.choice(colorList)
+        t.color(myColor)
+        t.fd(50)
+        t.lt(24)
+
+
 if __name__ == '__main__':
     import turtle
 

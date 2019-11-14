297f34ce19439f273c71c52e11b4e2f77a06c768
Adi Bhaktaram <adithyabhaktaram@casady.org>
2019-11-14 01:24:58 -0600

    Learned about Inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,7 +1,8 @@
+import random
 class  Card:
     '''Represents a standard playing card.'''
 
-    def __init__(self, suit = 0, ranke = 2):
+    def __init__(self, suit = 0, rank = 2):
         self.suit = suit
         self.rank = rank
 
@@ -12,9 +13,59 @@ class  Card:
     def __str__(self):
         return '%s of %s' % (Card.rank_names[self.rank], Card.suit_names[self.suit])
 
-    
+    def __lt__(self, other):
+        #check the suits
+        if self.suit < other.suit: return True
+        if self.suit > other.suit: return False
 
+        #suits are the same... check ranks
+        return self.rank < other.rank
+
+class Deck:
+    '''Represents a deck of cards'''
+
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def pop_card(self):
+        return self.cards.pop()
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def sort(self):
+        pass
+
+    def move_cards(self, hand, num):
+        for i in range(num):
+            hand.add_card(self.popcard())
+
+class Hand(Deck):
+    '''Represents a hand of playing cards'''
+
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
 
 
 if __name__ == '__main__':
-    queen_of_diamonds = Cards(1, 12)
+    hand = Hand('new hand')
+    hand.cards
+    hand.label
+    deck = Deck()
+    card = deck.pop_card()
+    hand.add_card(card)
+    print(hand)
